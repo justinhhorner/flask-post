3ccc5976efd3befacbdea5fca42b484b599478aa
Justin Horner <justinh.horner@gmail.com>
2023-02-12 00:20:54 -0500

    Add Bootstrap. Add user and error routes with templates

--- hello.py
+++ hello.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, request, make_response, abort, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
+
+@app.route('/agent')
+def agent():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.route('/cookie')
+def set_cookie():
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('505.html'), 500
