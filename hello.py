8eb3b7d4ed0c2a44046afad79f98499c66aa92b6
Justin Horner <justinh.horner@gmail.com>
2023-02-12 02:24:40 -0500

    Add WTForms, redirect, and flash messages

--- hello.py
+++ hello.py
@@ -1,15 +1,30 @@
 from datetime import datetime
-from flask import Flask, request, make_response,  render_template
+from flask import Flask, redirect, request, make_response,  render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm 
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '2e7d7490eaf58b49c3626c644dd9753b'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/agent')
 def agent():
