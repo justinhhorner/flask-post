bfa831e68c997dd56a8238c548952eb144668b07
Justin Horner <justinh.horner@gmail.com>
2023-02-12 00:51:49 -0500

    Add Moment extension

--- hello.py
+++ hello.py
@@ -1,12 +1,15 @@
-from flask import Flask, request, make_response, abort, render_template
+from datetime import datetime
+from flask import Flask, request, make_response,  render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/agent')
 def agent():
