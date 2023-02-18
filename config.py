e27bd4f00b39e5fa33ad0b8ad71cbcf59cbc5a4f
Justin Horner <justinh.horner@gmail.com>
2023-02-18 04:09:09 -0500

    Update config and fix typo in main init

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ class Config:
     ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flask Post]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flaskpost@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASK_POST_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
