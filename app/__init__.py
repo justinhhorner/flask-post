5580f3e2f1113cbc85c7bd4aa4f1e4739dd264e9
Justin Horner <justinh.horner@gmail.com>
2023-02-24 01:56:52 -0500

    Add initial migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -17,7 +17,6 @@ def create_app(config_name):
     db.init_app(app)
     
     # attach routes and custom error pages here
-
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, session, redirect, url_for
-from . import main
 from .forms import NameForm
+from . import main
 from .. import db
 from ..models import User
 
@@ -18,5 +18,5 @@ def index():
             session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
